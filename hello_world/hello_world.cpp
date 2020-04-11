70dfc04d28d897bd0da995c2546d0828f9c42862
xxxxxqqqqq <melih.guldogus@gmail.com>
2020-04-11 12:29:32 +0200

    some small changes

--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -4,6 +4,7 @@ int main()
 {
     #pragma omp parallel
     {
+        // Gets the thread (we dont know which one) -> It runs for each thread available
         int ID = omp_get_thread_num();
         printf("hello(%d)", ID);
         printf("world(%d)\n", ID);
